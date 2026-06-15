ca3e8731c28f26331f20d53d3f73b1ff17599c8a
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-06-15 10:59:30 +0900

    feat : 디버그 유틸리티 작성
    UDebug.cs 작성

---
+++ Assets/_Base/Script/Global/Utility/Extension/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+/// <summary>
+/// 문자열 확장 메서드를 담는 유틸리티
+/// </summary>
+public static class StringExtension
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsBlank(this string str)
+    {
+        return string.IsNullOrWhiteSpace(str);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsNotBlank(this string str)
+    {
+        return !IsBlank(str);
+    }
+}
