887983ed7563e7af4f5c84967b25251299033532
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-08-08 13:56:30 +0900

    Feat : Singleton, Frameable 등 뼈대 스크립트 작성

---
+++ Assets/_Project/Default/Scripts/Base/Frameable/IUpdateFrameable.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// FrameManager에게 가입당할 스크립트가 준수하는 인터페이스
+/// </summary>
+public interface IUpdateFrameable
+{
+    EUpdatePriority UpdatePriority { get; }
+    void ExecuteUpdateFrame();
+}
