bd8db9552d84425071f34c4af9900de7e6bda1c2
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-08-09 05:31:16 +0900

    Feat : 하이어라키 구분선 오브젝트 프리팹 작성

---
+++ Assets/_Project/Default/Hierarchy/CHierarchySeparator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+
+namespace Project.Default
+{
+    public class CHierarchySeparator : AMono
+    {
+#if UNITY_EDITOR
+        private const string SEPARATOR_NAME = "──────────────";
+        private void OnValidate()
+        {
+            EditorApplication.delayCall += SetGameObjectName;
+        }
+        private void SetGameObjectName()
+        {
+            if (this == null || gameObject == null) return;
+
+            if (gameObject.name != SEPARATOR_NAME)
+            {
+                gameObject.name = SEPARATOR_NAME;
+            }
+        }
+    }
+#endif
+}
